0f5ed9e98f6c78196448f2fba052c7567092cb77
vatsal <vso.opash@gmail.com>
2024-01-01 15:56:58 +0530

    change icons

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -80,6 +80,8 @@ import {
   faList,
   faCircle,
   faPhone,
+  faUsers,
+  faFileLines,
 } from '@fortawesome/free-solid-svg-icons';
 import { ClaimTokenModalComponent } from './modals/clai-1776-token-modal/claim-token-modal.component';
 import { WalletLinkComponent } from './modals/wallet-download-modal/1776-wallet.component';
@@ -218,7 +220,9 @@ export class SharedModule {
       faGlobe,
       faList,
       faCircle,
-      faPhone
+      faPhone,
+      faUsers,
+      faFileLines
     );
   }
 }
