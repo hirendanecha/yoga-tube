f3b5d491561be6ded596cafe0059ad6e5128a335
krp-opash <krp.opash@gmail.com>
2024-03-26 18:29:19 +0530

    Remove validation msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent {
             }
           },
           error: (err) => {
-            this.toastService.danger(err.error.message);
+            // this.toastService.danger(err.error.message);
             this.tokenService.signOut();
           },
         });
